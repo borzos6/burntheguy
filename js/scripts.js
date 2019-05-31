5cf5f64eed7bcdc25ff205c94444d1111bc5c000
László Békéssy <borzos6@gmail.com>
2019-05-31 09:30:20 +0200

    flameball changed to a bf rocket with flame animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-function shoot() {
-	let element = document.getElementById("fireball");
-	element.classList.add('flying');
+function launch(element) {
+  let rocketFlame = document.getElementById("rocket-flame");
+  rocketFlame.classList.remove('rocket-flame');
+  rocketFlame.classList.add('rocket-flame-flying');
+
+  let rocketBody = document.getElementById("rocket-body");
+  rocketBody.classList.remove('rocket-body');
+  rocketBody.classList.add('rocket-body-flying');
+
+  setInterval(function () {
+    if (!element.style.opacity) {
+      element.style.opacity = 1;
+    }
+    if (element.style.opacity > 0) {
+      element.style.opacity -= 0.1;
+    } else {
+      clearInterval(fadeEffect);
+    }
+  }, 50);
+
 }
